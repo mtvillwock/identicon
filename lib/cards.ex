26a358f70c606769c042f4519bad9660bbc6f1d5
Matthew Villwock <mtvillwock@gmail.com>
2019-07-20 18:24:21 -0700

    Add load method to load deck from file
    We saved the file in binary and we convert if from binary when loading.

--- lib/cards.ex
+++ lib/cards.ex
@@ -27,4 +27,9 @@ defmodule Cards do
     binary = :erlang.term_to_binary(deck)
     File.write(filename, binary)
   end
+
+  def load(filename) do
+    { status, binary } = File.read(filename)
+    :erlang.binary_to_term(binary)
+  end
 end
