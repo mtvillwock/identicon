95b7ac84f7505339ca861a421c014d6cf3ed209a
Matthew Villwock <mtvillwock@gmail.com>
2019-07-20 18:53:48 -0700

    Use tuples inside case statement for Cards.load
    Move pattern matching for reading file into case statement instead of
    beforehand.
    Indicate we aren't using `reason` variable.

--- lib/cards.ex
+++ lib/cards.ex
@@ -29,11 +29,9 @@ defmodule Cards do
   end
 
   def load(filename) do
-    { status, binary } = File.read(filename)
-
-    case status do
-      :ok -> :erlang.binary_to_term(binary)
-      :error -> "File #{filename} not found"
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term binary
+      {:error, _reason} -> "File #{filename} not found"
     end
   end
 end
